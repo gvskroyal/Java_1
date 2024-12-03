8fef8e2d6f7f2bd83181095b88a702ba3baa187d
gvskroyal <gvskroyal@gmail.com>
2024-12-03 11:05:27 +0530

    arrays int values

--- Arrays.java
+++ Arrays.java
@@ -1,11 +1,16 @@
 public class Arrays {
             public static void main(String[] args) {
         String[] cars={"bmw","maruthi","tata","lumborgini"};
+        int[] ages={25,30,35,40,45};
+        ages[0]=20;
         for(int i=0;i<=cars.length-1;i++){
             System.out.println(cars[i]);
         }
         System.out.println(cars.length);
-        
+        for(int i=0;i<=ages.length-1;i++){
+            System.out.println(ages[i]);
+        }
+        System.out.println(ages.length);
     }
     
 }
