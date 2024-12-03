2ae119a34111d48d6bdb301b877b64f51d72f037
gvskroyal <gvskroyal@gmail.com>
2024-12-03 10:54:44 +0530

    Arrays

---
+++ Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+            public static void main(String[] args) {
+        String[] cars={"bmw","maruthi","tata","lumborgini"};
+        for(int i=0;i<=cars.length-1;i++){
+            System.out.println(cars[i]);
+        }
+        System.out.println(cars.length);
+        
+    }
+    
+}
