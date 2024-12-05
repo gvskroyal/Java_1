49e738e76e0119a391e120112c53b80df3b1fe52
gvskroyal <gvskroyal@gmail.com>
2024-12-05 17:33:34 +0530

    Encapsulation

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String[] args) {
+        Encapsulation esn= new Encapsulation();
+        esn.setAge(25);
+        esn.setName("sai");
+        System.err.println(esn.getAge());
+        System.err.println(esn.getName());
+    }
+    
+}
